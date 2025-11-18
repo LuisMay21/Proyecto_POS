b8478f872134bc81b2e2fc6d2063eec01ca96161
LuisMay21 <123luismj@gmail.com>
2025-11-17 23:37:51 -0600

    Initial commint

---
+++ proyecto_pos/lib/Screens/splash_screen.dart
@@ -0,0 +1,27 @@
+import "dart:async";
+
+import "package:flutter/material.dart";
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    Timer(Duration(seconds: 3), () {});
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Image.asset('assets/images/logo.jpeg', width: 150, height: 150),
+      ),
+    );
+  }
+}
